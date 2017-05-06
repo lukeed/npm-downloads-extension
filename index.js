dd2404c35ad7baf1792ae1e264c9e859dfc8b0bc
Luke Edwards <luke.edwards05@gmail.com>
2017-05-06 10:54:10 -0700

    add html after last node element, regardless of desc existence

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
 	const API = 'https://api.npmjs.org/downloads/point/last-month';
 	const getCount = q => q && fetch(`${API}/${q}`).then(res => res.json());
 	const getLinks = () => [].slice.call(document.querySelectorAll(`${sel} a:not(.num)`));
-	const drawHTML = (el, num) => el.nextElementSibling.insertAdjacentHTML('afterend', ` - <em>(${num.toLocaleString()})</em>`);
+	const drawHTML = (el, num) => el.lastElementChild.insertAdjacentHTML('afterend', ` - <em>(${num.toLocaleString()})</em>`);
 	function main() {
 		const els = getLinks();
 		const all = els.map(e => e[tc]);
 		const query = all.filter(s => s.indexOf('@') === -1).join(',');
 		const scopes = all.filter(s => s.indexOf('@') !== -1).map(q => getCount(q).then(o => ({[q]:o})));
 		return Promise.all(scopes.concat(getCount(query))).then(arr => Object.assign.apply(null, arr)).then(obj => {
-			els.forEach(el => (el.className += ' num') && drawHTML(el, obj[el[tc]].downloads));
+			els.forEach(el => (el.className += ' num') && drawHTML(el.parentNode, obj[el[tc]].downloads));
 		});
 	}
 	setTimeout(main,1);
